bc13fedb64b07c6c0d23a0b90f94caf584a77f9c
Nayara Une <81695946+nayaraune@users.noreply.github.com>
2021-08-26 14:25:32 -0300

    Add files via upload

---
+++ projeto-controle-de-ponto/src/main/java/com/dio/projetocontroledeponto/ProjetoControleDePontoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.projetocontroledeponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControleDePontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControleDePontoApplication.class, args);
+	}
+
+}
---
+++ projeto-controle-de-ponto/src/test/java/com/dio/projetocontroledeponto/ProjetoControleDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.projetocontroledeponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoControleDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
